dbcf48a190ec2091a4d4be4be6b2be94c99a505d
yuankan20081 <176307920@qq.com>
2015-04-14 20:02:50 +0800

    split interfaces

--- session/session.go
+++ session/session.go
@@ -4,9 +4,17 @@ import (
 	"net"
 )
 
-type ISession interface {
+type OnConnecter interface {
 	OnConnect(addr net.Addr)
+}
+
+type OnDisconnecter interface {
 	OnDisconnect()
+}
+
+type ISession interface {
+	OnConnecter
+	OnDisconnecter
 	OnPacketParsed(data []byte)
 	ParsePacket(conn net.Conn, buffer []byte) ([]byte, error)
 }
